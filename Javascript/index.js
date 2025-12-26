0c783ba1e5337cdce2ad5ccfbfd76b7ac97ac6ba
sreenivasan <123955932+sreenivasan7624@users.noreply.github.com>
2025-12-26 20:14:00 +0530

    Create index.js

---
+++ Javascript/index.js
@@ -0,0 +1,63 @@
+// Task 1
+let a = "10";
+let b = 5;
+let result1 = a + b;
+console.log(result1);
+console.log(typeof result1);
+
+// Task 2
+let x = 10;
+let y = "10";
+console.log(x == y);
+console.log(x === y);
+
+// Task 3
+let num = 15;
+if (num > 0) {
+  console.log("Positive");
+} else if (num < 0) {
+  console.log("Negative");
+} else {
+  console.log("Zero");
+}
+
+// Task 4
+let p = 10;
+let q = 20;
+let r = "30";
+let result4 = p + q + r;
+console.log(result4);
+console.log(typeof result4);
+
+// Task 5
+let value = true;
+let result5 = Number(value) + 5;
+console.log(result5);
+console.log(typeof result5);
+
+// Task 6
+let a1 = 7;
+let b1 = 3;
+console.log(a1 > b1);
+
+// Task 7
+let marks = "85";
+console.log(Number(marks) >= 40);
+
+// Task 8
+let age = 20;
+let hasID = false;
+console.log(age >= 18 && hasID);
+
+// Task 9
+let count;
+console.log(count, typeof count);
+count = 10;
+console.log(count, typeof count);
+
+// Task 10
+let num1 = "5";
+let num2 = "2";
+console.log(num1 + num2, typeof (num1 + num2));
+console.log(num1 - num2, typeof (num1 - num2));
+console.log(num1 * num2, typeof (num1 * num2));
